134ee3dbd24da5731001e6a5f32aaf83120a4686
rifathossain112 <mdrifatssain112@gmail.com>
2026-01-19 02:24:52 +0600

    Added payoo mobile app project

---
+++ payoo mobile app/script.js
@@ -0,0 +1,17 @@
+document.getElementById("loginButton").addEventListener("click", function(e){
+    e.preventDefault();
+    const  mobileNumber =01832075595
+    const pin =1234
+    const mobileNumberValue = document.getElementById("mobile-number").value 
+    const mobileNumberValueConverted = parseInt(mobileNumberValue)
+    
+    const pinNumberValue = document.getElementById("pin-number").value
+    const pinNumberValueConverted = parseInt(pinNumberValue)
+    if(mobileNumberValueConverted === mobileNumber && pinNumberValueConverted === pin){
+        window.location.href="home.html"
+    }
+    else{
+        alert("Invalid Credentials")
+    }
+}
+)
